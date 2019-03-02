1fee5414564e91a973394ca98aa6ce53a80071bf
song <songgalmegi@naver.com>
2019-03-02 12:37:03 +0900

    Life Cycle - Stopwatch component2

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -14,12 +14,13 @@ export class Stopwatch extends React.Component {
 
   // life cycle
   // Dom이 생성된 직후
+  // 서드파트 라이브러리 실행 or 네트워크 호출 할때 필요
   componentDidMount() {
     this.tickRef = setInterval(this.tick, 1000);
   }
 
   tick = () => {
-    if(this.state.isRunning) {
+    if (this.state.isRunning) {
       this.setState(prevState => ({timer: prevState.timer + 1}))
     }
   };
@@ -39,7 +40,7 @@ export class Stopwatch extends React.Component {
         <h2>Stopwatch</h2>
         <span className="stopwatch-time">{this.state.timer}</span>
         <button onClick={this.handleStopwatch}>{this.state.isRunning ? 'Stop' : 'Start'}</button>
-        <button>Reset</button>
+        <button onClick={() => this.setState({timer: 0})}>Reset</button>
       </div>
     );
   }
