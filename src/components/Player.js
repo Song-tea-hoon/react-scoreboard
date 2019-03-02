6435c4feb47d0927b616ffee12274bb47c83d596
song <songgalmegi@naver.com>
2019-03-02 14:43:52 +0900

    PureComponent

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import {Counter} from "./Counter";
 
-export const Player = (props) => {
-  return (
-    <div className="player">
-    <span className="player-name">
-      <button className="remove-player" onClick={() => props.removePlayer(props.id)}>X</button>
-    </span>
-      <span className="player-name">{props.name}</span>
-      {/*
-        <Counter score={props.score}
-               index={props.index}
-               changeScore={(delta) => props.changeScore}/>
-        // object이름과 값을 그대로 spread해서 넘긴다..
-      */}
-      <Counter {...props}/>
-    </div>
-  );
-};
+export class Player extends React.PureComponent { // PureComponent props의 값이 변할 때에만 render를 하는 Component
+  render() {
+    console.log(this.props.name, ' rendered');
+    return (
+      <div className="player">
+      <span className="player-name">
+        <button className="remove-player" onClick={() => this.props.removePlayer(this.props.id)}>x</button>
+      </span>
+        <span className="player-name">
+        {this.props.name}
+      </span>
+        <Counter score={this.props.score} index={this.props.index} changeScore={this.props.changeScore} />
+      </div>
+    );
+  }
+  // // React.Component에서 render가 실행되기 직전에 호출되는 메서드
+  // // (true를 리턴하면 랜더링되고 false이면 랜더링 되지 않는다.)
+  // shouldComponentUpdate(nextProps, nextState, nextContext) {
+  //   return true;
+  // }
+}
