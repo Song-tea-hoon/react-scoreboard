04644e7b1e348ecf762946a208b3af1eb6b4e459
song <songgalmegi@naver.com>
2019-03-09 14:57:45 +0900

    scoreboard refactorying : container dom추가

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -17,15 +17,18 @@ export class Root extends React.Component {
       <BrowserRouter>
         {/*<Fragment> 는 <>로 축약 가능*/}
         <>
-          <Menu />
+          <Menu/>
           {/* switch는 일치하는 path의 페이지를 노출한다.
               그런데 path의 string의 맨앞부터 비교를 하는데 "/heros"에도 "/"가 포함되므로
               Home에서 일치한다고 판단 항상 Home을 노출한다.
               그래서 exact를 추가하여 path가 완벽히 일치하는 메뉴 하나만 노출하도록 한다. */}
+
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/heros" component={Heroes} />
-            <Route exact path="/scoreboard" component={Scoreboard} />
+            <div className="container" style={{backgroundColor: '#fff'}}>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/heros" component={Heroes}/>
+              <Route exact path="/scoreboard" component={Scoreboard}/>
+            </div>
           </Switch>
         </>
       </BrowserRouter>
