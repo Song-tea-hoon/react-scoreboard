0e7e070e67c54f8a192dd8416f403eacfd44bbc7
song <songgalmegi@naver.com>
2019-03-09 16:21:44 +0900

    heroes scss적용 scoped module은 아직 동작하지 않는다.

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {Home} from "./Home";
-import {Heroes} from "./Heroes";
+import {Heroes} from "./heroes/Heroes";
 import Scoreboard from "./scoreboard/Scoreboard";
 import {Menu} from "./Menu";
 
@@ -22,14 +22,13 @@ export class Root extends React.Component {
               그런데 path의 string의 맨앞부터 비교를 하는데 "/heros"에도 "/"가 포함되므로
               Home에서 일치한다고 판단 항상 Home을 노출한다.
               그래서 exact를 추가하여 path가 완벽히 일치하는 메뉴 하나만 노출하도록 한다. */}
-
-          <Switch>
-            <div className="container" style={{backgroundColor: '#fff'}}>
+          <div className="container" style={{backgroundColor: '#fff'}}>
+            <Switch>
               <Route exact path="/" component={Home}/>
               <Route exact path="/heros" component={Heroes}/>
               <Route exact path="/scoreboard" component={Scoreboard}/>
-            </div>
-          </Switch>
+            </Switch>
+          </div>
         </>
       </BrowserRouter>
     );
---
+++ src/pages/heroes/Heroes.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import axios from 'axios';
+// scss파일명에 .module을 붙이면 모듈방식으로 전역이 아니라 scoped scss가 된다.
+// https://facebook.github.io/create-react-app/docs/adding-a-css-modules-stylesheet
+import './Heroes.module.scss';
+
+export class Heroes extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  state = {
+    heroes: []
+  }
+
+  render() {
+    return (
+      <ul className="img-box">
+        {
+          this.state.heroes.map(hero => (
+            <li key={hero.hero_id} className="row align-items-center m-0">
+              <div className="col-1 py-2">
+                <img src={hero.photo} alt={hero.name} className="img-fluid rounded-circle" />
+              </div>
+              <span className="col">{hero.name}</span>
+            </li>
+          ))
+        }
+      </ul>
+    );
+  }
+
+  componentDidMount() {
+    this.getHeroes();
+  }
+
+  getHeroes = async () => {
+    const response = await axios.get('http://eastflag.co.kr:8080/api/heroes');
+    this.setState({heroes: response.data})
+  }
+}
