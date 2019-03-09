1edf8c10f9f24647fae7a0d0cde2b90cd71e13d1
song <songgalmegi@naver.com>
2019-03-09 15:14:13 +0900

    promise1 - then

--- js/promise1.js
+++ js/promise1.js
@@ -11,3 +11,15 @@ let promise = new Promise(resolve => {
 });
 //Promise 생성자안의 펑션은 executor로써 선언과 동시에 실행된다.
 //pending 상태 => resolve 상태 => settled 상태
+
+promise.then(value => {
+  console.log(value)
+  return new Promise( resolve => {
+    setTimeout( () => {
+      console.log('async2');
+      resolve('success2')
+    }, 1000 )
+  })
+}).then(value => {
+  console.log(value)
+});
