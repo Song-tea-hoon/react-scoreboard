c4bd1f8faa444ecb2ca210a43a86978b5d0cae1f
song <songgalmegi@naver.com>
2019-03-09 16:48:00 +0900

    css module - 다중 class

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {changeScore} from "../redux/actions";
 import{connect} from "react-redux";
+import styles from '../pages/scoreboard/Scoreboard.module.css'
 
 class Counter extends React.Component {
   // static 키워드를 이용하여 class내부에 prop-types 선언가능하다.
@@ -13,9 +14,11 @@ class Counter extends React.Component {
 
   render() {
     const {changeScore, score, index} = this.props;
+    const button1 = styles["counter-action"] + ' ' + styles.decrement;
+
     return (
-      <div className="counter">
-        <button className="counter-action" onClick={()=>changeScore(index, -1)}>-</button>
+      <div className={styles.counter}>
+        <button className={button1} onClick={()=>changeScore(index, -1)}>-</button>
         <span className="counter-score">{score}</span>
         {/* 이벤트의 등록은 명령형이 아니라 선언형 스타일로 등록해야한다. */}
         <button className="counter-action" onClick={()=>changeScore(index, 1)}>+</button>
