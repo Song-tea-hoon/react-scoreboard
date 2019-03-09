3f5a9a45262ba90edb1ec9a1638bb55e2fb2a97e
song <songgalmegi@naver.com>
2019-03-09 15:26:26 +0900

    async/await - 다중 await 및 then으로 결과 값 리턴받는 방법

--- js/async_await1.js
+++ js/async_await1.js
@@ -14,11 +14,13 @@ function resolveAfter2Sconds(x) {
 //   })
 
 async function add1(x) {
-  const result = await resolveAfter2Sconds(x);
-  console.log(result)
-  return result;
+  const result = await resolveAfter2Sconds(20);
+  console.log(result);
+  const result2 = await resolveAfter2Sconds(30);
+  console.log(result2)
+  return x + result + result2;
 }
 
-console.log(add1(10))
+console.log(add1(10).then( value => console.log('add1: ', value)));
 
 console.log('end');
